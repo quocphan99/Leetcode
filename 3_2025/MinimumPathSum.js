63bc42a875da7b0f28f00659e63f6ad36840ec8b
quocphan99 <tanquoc1999s@gmail.com>
2025-05-09 16:20:48 +0700

    update

--- 3_2025/MinimumPathSum.js
+++ 3_2025/MinimumPathSum.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    let dp = new Array(grid.length).fill(0).map(() => new Array(grid[0].length).fill(0));
-    dp[0][0] = grid[0][0];
+    let dp = new Array(grid[0].length);
+    dp[0] = grid[0][0];
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-
-            if (i === 0 && j === 0) {
-                continue;
-            }
-
-            if (i === 0) {
-                dp[i][j] = dp[i][j - 1] + grid[i][j];
-            } else if (j === 0) {
-                dp[i][j] = dp[i - 1][j] + grid[i][j];
-            } else {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-            }
-        }
+    for (let j = 1; j < grid[0].length; j++) {
+        dp[j] = dp[j - 1] + grid[0][j];
     }
 
-    return dp[grid.length - 1][grid[0].length - 1];
+    // console.log(dp);
+
+    for (let i = 1; i < grid.length; i++) {
+
+        dp[0] += grid[i][0];
+
+        for (let j = 1; j < grid[0].length; j++) {
+            dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
+        }
+
+        // console.log(dp);
+    }
+
+    return dp[dp.length - 1];
 };
 
-let grid = [[1,3,1],[1,5,1],[4,2,1]];
+let grid = [[1,2,3],[4,5,6]]
 console.log(minPathSum(grid)); // 7
